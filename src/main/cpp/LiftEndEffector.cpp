408800a2af3a1e9529d999f03aaa19409fcfd0cd
taichichuan <mike@theptrgroup.com>
2019-03-10 20:46:11 -0700

    Created thread to handle the lift.  Still working

--- src/main/cpp/LiftEndEffector.cpp
+++ src/main/cpp/LiftEndEffector.cpp
@@ -5,31 +5,84 @@
  *      Author: Mike A.
  */
 #include <LiftEndEffector.h>
-#include <frc/DigitalInput.h>
-#include <frc/Timer.h>
-#include "Ports.h"
+
+#ifdef EDV21
+WPI_VictorSPX* m_LiftMotor;
+#else
+WPI_TalonSRX* m_LiftMotor;
+#endif
 
 LiftEndEffector* LiftEndEffector::INSTANCE = nullptr;
+frc::DigitalInput* bottomLS;
+frc::DigitalInput* liftLS;
+frc::Timer* liftTimer;
+sem_t lifterSem;
+int numClicks = 0;
+bool threadIsActive = false;
+bool movementComplete = false;
+bool abortThreadIteration = false;
+
+#if defined(__linux__)
+static void LifterThread() {
+  while (true) {
+    sem_wait(&lifterSem);               // Wait until we're needed
+    while (numClicks > 0) {             // If we have clicks to go
+      if (!abortThreadIteration) {      // Check to see if we need to bail out
+        while (!(liftLS->Get())) {      // Check to see if switch is triggered
+          if (!abortThreadIteration) {  // Check to see if we need to bail out
+            frc::Wait(0.2);             // check every .2 seconds
+          } else {
+            LiftEndEffector::manualLiftStop();  // We got abort so stop the lift
+            numClicks = 0;  // Let's bail out of looking for clicks
+            break;
+          }
+        }
+        numClicks--;  // decrement the click counter
+      } else {
+        LiftEndEffector::manualLiftStop();  // We got abort so stop the lift
+        numClicks = 0;
+        break;
+      }
+    }
+  }
+}
+#endif
 
 // If cargo is extended, then we're dealing with hatches
 // if cargo is retracted, then were dealing with cargo
 
 LiftEndEffector::LiftEndEffector() {
   // put initialization code into the constructor
-  LiftEndEffector::liftLS = new frc::DigitalInput(RobotPorts::kLiftLimSw);
-  LiftEndEffector::bottomLS = new frc::DigitalInput(RobotPorts::kBottomLimSw);
+  liftLS = new frc::DigitalInput(RobotPorts::kLiftLimSw);
+  bottomLS = new frc::DigitalInput(RobotPorts::kBottomLimSw);
   LiftEndEffector::liftPos = BOTTOM;
-  LiftEndEffector::liftTimer = new frc::Timer();
+  liftTimer = new frc::Timer();
 
+  try {
+    liftEF = LiftEndEffector::getInstance();
+  } catch (std::exception& e) {
+    frc::DriverStation::ReportError("Error initializing OI object");
+    frc::DriverStation::ReportError(e.what());
+  }
+
+#if defined(__linux__)
+  std::thread lifterThread(LifterThread);
+  lifterThread.detach();
+#else
+  wpi::errs() << "Vision only available on Linux.\n";
+  wpi::errs().flush();
+#endif
+
+#ifdef EDV21
+  m_LiftMotor = new WPI_VictorSPX(RobotPorts::kLiftChannel);
+#else
+  m_LiftMotor = new WPI_TalonSRX(RobotPorts::kLiftChannel);
+#endif
 }
 
-void LiftEndEffector::liftTimerReset() {
-  LiftEndEffector::liftTimer->Reset();
-}
+void LiftEndEffector::liftTimerReset() { liftTimer->Reset(); }
 
-void LiftEndEffector::liftTimerStart() {
-  LiftEndEffector::liftTimer->Start();
-}
+void LiftEndEffector::liftTimerStart() { liftTimer->Start(); }
 
 void LiftEndEffector::liftUp(int numSwitches, bool disableSensor) {
   // without the sensor, this does nothing
@@ -56,7 +109,8 @@ void LiftEndEffector::bottomPos(bool disableSensor) {
   int numSwitchesToGo = 0;
   if (!disableSensor) {
     if (LiftEndEffector::liftPos > BOTTOM) {
-      numSwitchesToGo = LiftEndEffector::liftPos;  // This is the number of switches to go
+      numSwitchesToGo =
+          LiftEndEffector::liftPos;  // This is the number of switches to go
       if (!disableSensor) {
         // we need to track the number of sensor clicks
         while (!(liftLS->Get())) {
@@ -80,18 +134,18 @@ void LiftEndEffector::cargoPos(int numSwitches, bool disableSensor) {
 }
 
 void LiftEndEffector::manualLiftUp() {
-  m_LiftMotor.Set(-LiftEndEffector::liftMotorSpeed);
+  m_LiftMotor->Set(-LiftEndEffector::liftMotorSpeed);
   printf("Liftup\n");
 }
 
 void LiftEndEffector::manualLiftDown() {
-  m_LiftMotor.Set(LiftEndEffector::liftMotorSpeed);
+  m_LiftMotor->Set(LiftEndEffector::liftMotorSpeed);
   printf("LiftDown\n");
 }
 
 void LiftEndEffector::manualLiftStop() {
   printf("LiftStop\n");
-  m_LiftMotor.Set(0.0);
+  m_LiftMotor->Set(0.0);
 }
 
 LiftEndEffector* LiftEndEffector::getInstance() {
--- src/main/include/LiftEndEffector.h
+++ src/main/include/LiftEndEffector.h
@@ -6,43 +6,48 @@
  */
 #ifndef SRC_LIFTENDEFFECTOR_H_
 #define SRC_LIFTENDEFFECTOR_H_
-#include <frc/DigitalInput.h>
-#include <ctre/Phoenix.h>
+
 #include <Ports.h>
+#include <ctre/Phoenix.h>
+#include <frc/DigitalInput.h>
+#include <frc/DriverStation.h>
+#include <frc/Timer.h>
+#include <semaphore.h>
 
 class LiftEndEffector {
-public:
-    static LiftEndEffector* getInstance();
-
-#ifdef EDV21    
-     WPI_VictorSPX m_LiftMotor{RobotPorts::kLiftChannel};
-#else
-     WPI_TalonSRX m_LiftMotor{RobotPorts::kLiftChannel};
-#endif
+ public:
+  static LiftEndEffector *getInstance();
 
   const double liftMotorSpeed = 1.0;
 
-  enum liftPosition {BOTTOM, LOADER, RKTBTMCARGO, CARGOBAY, RKTMIDHATCH, RKTMIDCARGO, RKTTOPHATCH, RKTTOPCARGO};
+  enum liftPosition {
+    BOTTOM,
+    LOADER,
+    RKTBTMCARGO,
+    CARGOBAY,
+    RKTMIDHATCH,
+    RKTMIDCARGO,
+    RKTTOPHATCH,
+    RKTTOPCARGO
+  };
 
-  liftPosition liftPos = BOTTOM;  // Start at the bottom position at the beginning of the match
-
-  frc::DigitalInput *bottomLS;
-  frc::DigitalInput *liftLS;
-  frc::Timer *liftTimer;
+  liftPosition liftPos =
+      BOTTOM;  // Start at the bottom position at the beginning of the match
 
   void liftUp(int numSwitches, bool disableSensor);
   void liftDown(int numSwitches, bool disableSensor);
   void manualLiftUp();
   void manualLiftDown();
-  void manualLiftStop();
+  static void manualLiftStop();
   void bottomPos(bool disableSensor);
   void cargoPos(int numSwitches, bool disableSensor);
   void liftTimerReset();
   void liftTimerStart();
 
-private:
-    LiftEndEffector();
-    static LiftEndEffector* INSTANCE;
+ private:
+  LiftEndEffector();
+  LiftEndEffector *liftEF;
+  static LiftEndEffector *INSTANCE;
 };
 
 #endif /* SRC_LIFTENDEFFECTOR_H_ */
